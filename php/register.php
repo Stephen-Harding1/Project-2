36567cca838ce850d4fda948a90b7a65de1bc8b4
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-11-12 20:49:29 -0500

    Changed the code
    I recently it from html to php and added more php files to make the login form work

--- php/register.php
+++ php/register.php
@@ -1,3 +1,12 @@
+<?php
+require_once "config.php";
+require_once "session.php";
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
+    $fullname = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
